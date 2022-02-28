5c0bf56452303b9b7a4656c6fd0a50da25c26324
The Luca <80943931+Vacation-Time@users.noreply.github.com>
2022-02-28 12:18:45 -0800

    Completed Prob 2

--- app.js
+++ app.js
@@ -104,22 +104,22 @@ let dishes = [
 ]
 
 // Example function
-// function findMexicanFood(){
-//     let results = dishes.filter(function(el){
-//         if(el.cuisine === "Mexican"){
-//             return true;
-//         }
-//         else{
-//             return false;
-//         }})
+function findMexicanFood(){
+    let results = dishes.filter(function(el){
+        if(el.cuisine === "Mexican"){
+            return true;
+        }
+        else{
+            return false;
+        }})
 
-//     //Debug tip: Place a Breakpoint on the return AFTER the filter, then hover over "results" to see results of filter without having to step through the whole thing!
+    //Debug tip: Place a Breakpoint on the return AFTER the filter, then hover over "results" to see results of filter without having to step through the whole thing!
 
-//     return results;
-// }
+    return results;
+}
 
-// let mexicanFood = findMexicanFood();
-// console.log('Mexican Foods: ', mexicanFood)
+let mexicanFood = findMexicanFood();
+console.log('Mexican Foods: ', mexicanFood)
 
 
 
@@ -144,6 +144,22 @@ console.log('Vegetarian Foods: ', vegetarianFood)
 
 //2. Create a function that will return all dishes with the cuisine type of "Italian" and a serving size greater than 5.
 //Filter
+
+function findItalianFood(){
+    let results = dishes.filter(function(el){
+        if(el.cuisine === "Italian" && el.servings >= 5){
+            return true;
+        }
+        else{
+            return false;
+        }})
+
+
+    return results;
+}
+
+let italianFood = findItalianFood();
+console.log('Italian Foods w/serving size > 5: ', italianFood)
 
 //3. Create a function that will return only dishes whose serving id number matches their serving count.
 //Filter
