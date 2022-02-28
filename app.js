67263d1db55bdeaa9cf459997cbc1c70f154fc4b
The Luca <80943931+Vacation-Time@users.noreply.github.com>
2022-02-28 12:25:45 -0800

    Completed Prob 3

--- app.js
+++ app.js
@@ -164,6 +164,22 @@ console.log('Italian Foods w/serving size > 5: ', italianFood)
 //3. Create a function that will return only dishes whose serving id number matches their serving count.
 //Filter
 
+function findIdMatchingServingsCountFood(){
+    let results = dishes.filter(function(el){
+        if(el.id ===  el.servings){
+            return true;
+        }
+        else{
+            return false;
+        }})
+
+
+    return results;
+}
+
+let servingsAndIdMatching = findIdMatchingServingsCountFood();
+console.log('Foods Serving size matchig ID: ', servingsAndIdMatching)
+
 //4. Create a function that will return only dishes whose serving count is even.
 //Filter
 
